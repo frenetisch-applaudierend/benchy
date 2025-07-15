fc68a39f7774476e1f4e019b49885a21b1207917
Markus Gasser <markus.gasser@mailbox.org>
2025-07-15 22:17:40 +0000

    Expand collected statistics

--- Benchy/Core/BenchmarkReport.cs
+++ Benchy/Core/BenchmarkReport.cs
@@ -7,9 +7,51 @@ public sealed record BenchmarkReport(
     IReadOnlyList<BenchmarkReport.Benchmark> Benchmarks
 )
 {
-    public sealed record Benchmark(string FullName, Statistics Statistics);
+    public sealed record Benchmark(string FullName, Statistics Statistics, MemoryMetrics Memory);
 
-    public sealed record Statistics(double Mean);
+    public sealed record Statistics(
+        double Mean,
+        double Min,
+        double Max,
+        double Median,
+        double StandardDeviation,
+        double StandardError,
+        double Variance,
+        double Skewness,
+        double Kurtosis,
+        ConfidenceInterval ConfidenceInterval,
+        Percentiles Percentiles
+    );
+
+    public sealed record MemoryMetrics(
+        int BytesAllocatedPerOperation,
+        int Gen0Collections,
+        int Gen1Collections,
+        int Gen2Collections,
+        long TotalOperations
+    );
+
+    public sealed record ConfidenceInterval(
+        int N,
+        double Mean,
+        double StandardError,
+        int Level,
+        double Margin,
+        double Lower,
+        double Upper
+    );
+
+    public sealed record Percentiles(
+        double P0,
+        double P25,
+        double P50,
+        double P67,
+        double P80,
+        double P85,
+        double P90,
+        double P95,
+        double P100
+    );
 
     public static IEnumerable<BenchmarkReport> LoadReports(DirectoryInfo directory)
     {
